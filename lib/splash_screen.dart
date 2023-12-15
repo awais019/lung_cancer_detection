8ec957fdf72de474cac1ace3a4fca91a7b00af35
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-12-15 16:17:17 +0500

    feat: splash screen
    Added splash screen with animated splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        duration: 3000,
+        splash: const Text(
+          'Lung Sense',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        splashTransition: SplashTransition.decoratedBoxTransition,
+        animationDuration: const Duration(milliseconds: 1000),
+        nextScreen: const Text('Lung cancer detection'));
+  }
+}
